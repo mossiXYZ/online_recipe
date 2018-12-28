89a51679fadf3c7d3890eb9611a7d129f5173eaa
mossi <mossi.hashemi@gmail.com>
2018-12-28 16:01:34 +0100

    first mileston

--- src/app/recpies/recpie-list/recpie-list.component.ts
+++ src/app/recpies/recpie-list/recpie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recpie } from '../../shared/models/recpie.model';
 
 @Component({
   selector: 'app-recpie-list',
@@ -7,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecpieListComponent implements OnInit {
 
+  recpies : Recpie[] =[
+    new Recpie('دستور پخت ماکارونی',
+    'توضیحات برای پخت ماکارونی مامانپذ',
+    'https://thestayathomechef.com/wp-content/uploads/2017/12/Pasta-Pomodoro-4-small.jpg'),
+    new Recpie('دستور پخت قرمه سبزی',
+    'توضیحات برای پخت قرمه سبزی مامانپذ',
+    'https://www.2nafare.com/wp-content/uploads/2015/01/%D9%82%D9%88%D8%B1%D9%85%D9%87-%D8%B3%D8%A8%D8%B2%DB%8C-%D8%AC%D8%A7-%D8%A7%D9%81%D8%AA%D8%A7%D8%AF%D9%87.jpg'),
+    new Recpie('دستور پخت قیمه بادمجون',
+    'توضیحات برای پخت قیمه بادمجون مامانپذ',
+    'http://www.beytoote.com/images/stories/food/fo3897.jpg')
+  ]
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/shared/models/recpie.model.ts
@@ -0,0 +1,12 @@
+export class Recpie {
+
+    name: String;
+    description: String;
+    photoPath: String;
+
+    constructor(name: String, description: String, photoPath: String){
+        this.name = name;
+        this.description = description;
+        this.photoPath = photoPath;
+    }
+}
